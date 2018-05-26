51c51ffd3218e29086038fe658b64fa25007285c
Nate <natron-alias@hotmail.com>
2018-05-25 20:26:57 -0700

    add a sample ICommand in its own C# class

---
+++ FaceTutorial/FaceTutorial/ViewModel/ComplicatedSaveCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FaceTutorial.ViewModel
+{
+    class ComplicatedSaveCommand : ICommand
+    {
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            // TODO: add implementation here
+        }
+    }
+}
